6d77336ae28c136592e3a23b0458acddb8c988ca
F.Kaiser <21110785+le3tspeak@users.noreply.github.com>
2024-05-06 10:17:43 +0200

    S- MTh

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ internal static class Program
     /// <summary>
     ///  The main entry point for the application.
     /// </summary>
-    [STAThread]
+    [MTAThread]
     static void Main()
     {
         // To customize application configuration such as set high DPI settings or default font,
